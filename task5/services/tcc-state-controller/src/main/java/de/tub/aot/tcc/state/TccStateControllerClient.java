abbf68e4cda660de771c69a777cf2bdcb0cfbb20
jan_schaefer <jan.n.schaefer@campus.tu-berlin.de>
2026-01-29 21:45:18 +0100

    Updated Management Client to proper Endpoints

--- task5/services/tcc-state-controller/src/main/java/de/tub/aot/tcc/state/TccStateControllerClient.java
+++ task5/services/tcc-state-controller/src/main/java/de/tub/aot/tcc/state/TccStateControllerClient.java
@@ -11,6 +11,12 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import io.quarkus.oidc.client.filter.OidcClientFilter;
+import java.util.UUID;
+import java.util.ArrayList;
+
+import de.tub.aot.common.models.TrafficStatus;
+import de.tub.aot.common.models.TrafficLightId;
+
 
 @RegisterRestClient(configKey = "traffic-light-api")
 @OidcClientFilter
@@ -20,11 +26,25 @@ public interface TccStateControllerClient {
 
     @POST
     @Path("/api/device/change-state")
-    Response changeState(@QueryParam("state") String currentState);
+    @Produces(MediaType.APPLICATION_JSON)
+    Response changeState();
+
+    @POST
+    @Path("/api/device/management/change-state")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    Response managementChangeState(TrafficStatus goalStatus);
 
     @GET
     @Path("/api/device/traffic-state")
-    Response getTrafficState();
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    TrafficStatus getTrafficState(@QueryParam("traffic-light-id") UUID trafficLightId);
+
+    @GET
+    @Path("/api/device/traffic-lights")
+    @Produces(MediaType.APPLICATION_JSON)
+    ArrayList<TrafficLightId> getTrafficLights();
 
     // TODO: Add Other Client Endpoints to reach services internally
 }
